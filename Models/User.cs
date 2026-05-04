aa77802666ee19e4c7ebd7cb54a4e744db5d2725
Sabareesh001 <sabareeshcomp@gmail.com>
2026-05-04 20:31:42 +0530

    completed task

--- Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Oops.Models
-{
-    public class User(string name, string email, string phoneNumber)
-    {
-        public string Name { get; set; } = name;
-        public string Email { get; set; } = email;
-        public string PhoneNumber { get; set; } = phoneNumber;
-    }
-}
